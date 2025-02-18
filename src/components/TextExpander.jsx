ff092b6ec58e66f23750366d47d84795088309e2
Artem Trifonov <artemis.levitt@gmail.com>
2025-02-18 13:24:26 +0100

    Implement text collapse logic

--- src/components/TextExpander.jsx
+++ src/components/TextExpander.jsx
@@ -1,5 +1,25 @@
-function TextExpander() {
-    return <div>TODO</div>;
+import {useState} from "react";
+
+function TextExpander({collapsedNumWords,
+                          expandButtonText = "Show more",
+                          collapseButtonText = "Show less",
+                          buttonColor,
+                          expanded,
+                          className,
+                          children,
+                      }) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const displayText = isExpanded ? children : "temp";
+
+    return (
+        <div className={className}>
+            <span>{displayText}</span>
+            <button onClick={()=>setIsExpanded(!isExpanded)}>
+                {expanded ? collapseButtonText : expandButtonText}
+            </button>
+        </div>
+    )
 }
 
 export default TextExpander;
